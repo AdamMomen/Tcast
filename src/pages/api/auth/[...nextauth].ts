3a37d3862ca8b7cf7710094950e3545ea9cb5024
Victor Mimo <victormimoc@gmail.com>
2022-11-25 09:27:19 -0500

    adding version 2 of oauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,50 +1,47 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
-
 import { env } from "../../../env/server.mjs";
 
 type Account = {
-    provider?: string;
-    accessToken?: string;
-    refreshToken?: string
-}
-console.log(env)
+  provider?: string;
+  accessToken?: string;
+  refreshToken?: string;
+};
+console.log({ env });
 export const authOptions: NextAuthOptions = {
-    secret: env.NEXTAUTH_SECRET,
-    callbacks: {
-        async jwt(token, user, account: Account = {}, profile, isNewUser: boolean) {
-            if (!account.accessToken ||
-                !account.provider ||
-                !account.refreshToken) {
-                console.log("account is missing preperties")
-                return
-            }
+  secret: env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt(token, user, account: Account = {}, profile, isNewUser: boolean) {
+      if (!account.accessToken || !account.provider || !account.refreshToken) {
+        console.log("account is missing preperties");
+        return;
+      }
 
-            if (!token[account.provider]) {
-                token[account.provider] = {};
-            }
+      if (!token[account.provider]) {
+        token[account.provider] = {};
+      }
 
-            token[account.provider].accessToken = account.accessToken;
-            token[account.provider].refreshToken = account.refreshToken;
+      token[account.provider].accessToken = account.accessToken;
+      token[account.provider].refreshToken = account.refreshToken;
 
-            return token;
-        },
-        session({ session, user }) {
-            if (session.user) {
-                session.user.id = user.id;
-            }
-            return session;
-        },
+      return token;
     },
-    // Configure one or more authentication providers
-    providers: [
-        TwitterProvider({
-            version: "v2",
-            clientId: env.TWITTER_CLIENT_ID,
-            clientSecret: env.TWITTER_CLIENT_SECRET,
-        })
-    ],
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  providers: [
+    TwitterProvider({
+      clientId: env.TWITTER_CLIENT_ID,
+      clientSecret: env.TWITTER_CLIENT_SECRET,
+      version: "2.0",
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
